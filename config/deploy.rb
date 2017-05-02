d0fdc44e0cf22cef45aeef16bf51ec6f200966af
Andrii Shmyhovskyi <an.shmyg@yandex.ru>
2017-05-02 16:21:02 -0400

    commit for production

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,37 +1,29 @@
 # Change these
-server '54.93.105.124', port: 22, roles: [:web, :app, :db], primary: true
+server '54.214.116.213', port: 22, roles: [:web, :app, :db], primary: true
+
+#set :repo_url,        'git://github.com/anshmyg/rubyapp'
+set :user,            'deploy'
 
 set :repo_url,        'git://github.com/anshmyg/rubyapp'
 set :application,     'demo'
 set :user,            'deploy'
 set :puma_threads,    [4, 16]
-set :puma_workers,    1
+set :puma_workers,    0
 
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,	      '/var/demo'
-set :puma_bind,       "unix://#{fetch(:deploy_to)}/shared/tmp/sockets/puma_#{fetch(:application)}.sock"
-set :puma_state,      "#{fetch(:deploy_to)}/shared/tmp/sockets/puma.state"
-set :puma_pid,        "#{fetch(:deploy_to)}/shared/tmp/puma.pid"
-set :puma_access_log, "#{fetch(:deploy_to)}/log/puma.error.log"
-set :puma_error_log,  "#{fetch(:deploy_to)}/log/puma.access.log"
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-
-## Defaults:
-# set :scm,           :git
-# set :branch,        :master
-# set :format,        :pretty
-# set :log_level,     :debug
-# set :keep_releases, 5
-
-## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids'
